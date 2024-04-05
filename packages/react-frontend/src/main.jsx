4d85bc86a7d4801336b77bd94d4063f213ebdf4a
jamedw <jamesdwyer771@gmail.com>
2024-04-05 12:33:07 -0700

    removed starting files from /src and created main.css/jsx, Table.jsx and MyApp.jsx

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import MyApp from "./MyApp";
+import "./main.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const container = document.getElementById("root");
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<MyApp />);
